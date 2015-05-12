a93d5f528ce02f28de3156cd754d028454f046de
i-xiongqi <i-xiongqi@betterwood.com>
2015-05-12 20:09:07 +0800

    Deleted some useless data.Revised the DataGenerate file.

---
+++ DenStream/Data/DataGenerate/PointEntrance.m
@@ -0,0 +1,12 @@
+function[point] = PointEntrance(DATA)
+point.Coordinate = zeros(1000 , 3);
+point.Time = zeros(1000 , 1);
+point.Natural_Cluster_Name = zeros(1000 , 1);
+point = repmat(point, 200, 1);
+for i = 1 : 200
+    index = ((i-1)*1000+1):(i*1000);
+    point(i).Time = DATA.Time(index);
+    point(i).Coordinate = DATA.Coordinate(index,:);
+    point(i).Natural_Cluster_Name = DATA.Natural_Cluster_Name(index);
+end
+
