30d2a9b8faf0dac975db6fd8ee6cc55b23742b4b
XiongQi_Home <i-xiongqi@betterwood.com>
2015-05-25 08:59:45 +0800

    normal modified

--- DenStream/Class/Group.m
+++ DenStream/Class/Group.m
@@ -1,4 +1,4 @@
-classdef Group
+classdef Group < handle
     %GROUP Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -13,12 +13,12 @@ classdef Group
             obj.group(1,:) = [];
             obj.MCNumber = 0;
         end
-        function AddMC(obj,MC)
+        function obj = AddMC(obj,MC)
             obj.MCNumber = obj.MCNumber + 1;
             obj.group(obj.MCNumber,:).microCluster = MC;
             obj.groupMatrix = [obj.groupMatrix;MC.c];
         end
-        function DeleteMC(obj,index)
+        function obj = DeleteMC(obj,index)
             obj.group(index,:) = [];
             obj.groupMatrix(index,:) = [];
         end
--- DenStream/Class/MC.m
+++ DenStream/Class/MC.m
@@ -15,29 +15,31 @@ classdef MC < handle
         end
         function obj = Renew(obj,point)
             obj.weight = obj.weight + 1;
-            obj.CF1 = obj.CF1 + point.Coorinate;
+            obj.CF1 = obj.CF1 + point.Coordinate;
             obj.CF2 = obj.CF2 + (point.Coordinate*point.Coordinate');
+            obj = obj.CalculateC_R;
         end
         function obj = Decay(obj,time)
             decayRate = 1/(2^(PARA.LAMBDA*time));
             obj.weight = obj.weight*decayRate;
             obj.CF1 = obj.CF1*decayRate;
             obj.CF2 = obj.CF2*decayRate;
+            obj = obj.CalculateC_R;
         end
         function obj = CalculateC_R(obj)
             obj.c = obj.CF1/obj.weight;
-            obj.r = sqrt(obj.CF2/obj.weight-(obj.CF1/obj.weight)^2);
+            obj.r = sqrt(obj.CF2/obj.weight-(sum(obj.CF1/obj.weight).^2));
         end
         function Temp = SaveData(obj)
             Temp.weight = obj.weight;
             Temp.CF1 = obj.CF1;
             Temp.CF2 = obj.CF2;
         end
-        function Recover(obj,Temp)
+        function obj = Recover(obj,Temp)
             obj.weight = Temp.weight;
             obj.CF1 = Temp.CF1;
             obj.CF2 = Temp.CF2;
-            clear Temp;
+            obj = obj.CalculateC_R;
         end
             
     end
