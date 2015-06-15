de895a60c41aa4fc8b88c2ec67e89ae1169ec89e
i-xiongqi <i-xiongqi@betterwood.com>
2015-06-15 14:43:46 +0800

    HavaDone

--- DenStream/main/DenStream/DenStream.m
+++ DenStream/main/DenStream/DenStream.m
@@ -4,6 +4,7 @@ pmcGroup = PMCGroup;
 omcGroup = OMCGroup;
 pointFactory = PointFactory(pmcGroup,omcGroup,PARA);
 disp(PARA.T);
+color = 'ymcrgbwk';
 for i = 1:max(size(DATA))
     
     Point = PointInput(DATA,i).GetPoint;
@@ -21,6 +22,21 @@ for i = 1:max(size(DATA))
     end
     %The part "Merge" have done.
     pointFactory = pointFactory.DecayMC(PARA,time);
+    
+    [group_label,k] = DBSCAN_PMC( pmcGroup, 5, 3);
+    temp = pmcGroup.groupMatrix;
+    figure(i);
+    hold on;
+    color_index = 1;%ÑÕÉ«±êÇ©
+    for index=1:k
+        label = find(group_label == index);
+        if length(label) > length(group_label)/20
+            plot3(temp(label,1),temp(label,2),temp(label,3),'LineStyle','*','MarkerSize',5,'Color',color(color_index));
+            color_index = color_index + 1;
+        end
+    end
+    view(45,15)
+    hold off;
 end
 
 
